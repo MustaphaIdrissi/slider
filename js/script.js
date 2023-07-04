7c4a6a344afcf138e70cd9ad94589cb3b31c115b
Mustapha <56976902+MustaphaIdrissi@users.noreply.github.com>
2023-07-04 22:21:46 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const prev =document.querySelector(".prev");
+const next =document.querySelector(".next");
+const imgy =document.querySelectorAll(".igh");
+const sld =document.querySelector(".slider-image");
+
+
