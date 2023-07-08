bec65fda871064a93af6887c46ef9649208a4663
Mustapha <56976902+MustaphaIdrissi@users.noreply.github.com>
2023-07-08 12:25:33 +0100

    Update script.js
    sdqsd

--- js/script.js
+++ js/script.js
@@ -3,4 +3,48 @@ const next =document.querySelector(".next");
 const imgy =document.querySelectorAll(".igh");
 const sld =document.querySelector(".slider-image");
 
+var currentSlide=0;
+var slideInterval;
+next.addEventListener("click",()=>{
+    imgy[currentSlide].classList.remove("active");
+  currentSlide = (currentSlide + 1) % imgy.length;
+  imgy[currentSlide].classList.add("active");
+});
 
+prev.addEventListener("click",()=>{
+    imgy[currentSlide].classList.remove("active");
+  currentSlide = (currentSlide - 1+ imgy.length) % imgy.length;
+  imgy[currentSlide].classList.add("active");
+});
+
+
+
+
+
+function startSlideInterval() {
+    slideInterval =setInterval(function(){
+        imgy[currentSlide].classList.remove("active");
+        currentSlide = (currentSlide + 1) % imgy.length;
+        imgy[currentSlide].classList.add("active");
+    }, 3000);
+  }
+  
+  function pauseSlideInterval() {
+  
+   clearInterval(slideInterval);
+  }
+  
+  function handleMouseEnter() {
+    pauseSlideInterval();
+  }
+  
+  function handleMouseLeave() {
+    startSlideInterval();
+  }
+  
+  startSlideInterval();
+
+
+
+  sld.addEventListener("mouseenter", handleMouseEnter);
+  sld.addEventListener("mouseleave", handleMouseLeave);
